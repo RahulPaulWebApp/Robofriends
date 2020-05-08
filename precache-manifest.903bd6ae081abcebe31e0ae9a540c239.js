5665e108673e78a4c48a37d0fa8f32184d3e0cfb
rahul6373paul <57725754+rahul6373paul@users.noreply.github.com>
2020-05-08 20:57:07 +0530

    Add files via upload

---
+++ precache-manifest.903bd6ae081abcebe31e0ae9a540c239.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5a570d9e7022d2645eaa628b489baf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be9b6d369af1e13cc6f9",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "30a8c8c250880507be4d",
+    "url": "/static/css/main.93bae99b.chunk.css"
+  },
+  {
+    "revision": "be9b6d369af1e13cc6f9",
+    "url": "/static/js/2.f095a3df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f095a3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30a8c8c250880507be4d",
+    "url": "/static/js/main.444c7143.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
